3ecc66fe10517b6a587a74fdeac595325811b895
unknown <huutam.ptnk@gmail.com>
2018-09-04 09:26:17 +0300

    added correct solution

--- Kickstart Round A 2018/A/main.cpp
+++ Kickstart Round A 2018/A/main.cpp
@@ -1,47 +1,71 @@
 #include <iostream>
 #include <stdio.h>
+#include <string>
 using namespace std;
 long long n,m;
 int T;
 
-long long n8(long long k){
+long long toInt(string s){
     long long ret=0;
-    while(k/10>0){
-        ret=ret*10+8;
-        k=k/10;
+    for (int i=0;i<s.length();i++){
+        ret=ret*10+(s[i]-'0');
     }
     return ret;
 }
+
 int main()
 {
     freopen("input.inp","r",stdin);
     freopen("output.out","w",stdout);
     cin>>T;
+    string st,st1,st2;
     for (int t=1; t<=T; t++){
-        cin>>m;
-        n=m;
-        long long tg=0,tc=1,last_tc,last_tg=-1,last_n;
-        while(n>0){
-            if ((n%10)%2==1){
-                last_tc=tc;
-                last_tg=tg;
-                last_n=n;
+        cin>>st;
+        st1=st;
+        st2=st;
+        int kt=0;
+        // Get the lower num
+        for(int i=0;i<st.length();i++){
+            if ((st[i]-'0')%2==1 && kt==0){
+                st1[i]=st[i]-1;
+                kt=1;
             }
-            tg=(n%10)*tc+tg;
-            tc=tc*10;
-            n=n/10;
+            else if (kt==1){
+                st1[i]='8';
+            }
         }
-        printf("Case #%i: ",t);
-        if (m==1){
-            printf("1\n");
-            continue;
+        //cout<<st1<<endl;
+        int add=1;
+        // Get the higher num
+        for (int i=0; i<st.length();i++){
+            if ((st[i]-'0')%2==1){
+                if (st[i]!='9'){
+                    st2[i]++;
+                }
+                else{
+                    add=0;
+                    for(int j=i;j>=0;j--){
+                        if (st[j]!='8' && i!=j){
+                            st2[j]+=2;
+                            add=1;
+                            break;
+                        }
+                        st2[j]='0';
+                    }
+                }
+                for (int j=i+1;j<st.length();j++)
+                    if ((st[i]-'0')%2==1){
+                        st2[j]='0';
+                    }
+                break;
+            }
         }
-        if (last_tg==-1){
-            printf("0");
+        if (add==0){
+            st2="2"+st2;
         }
-        else{
-            printf("%lld",min(last_tc+last_tg-n8(last_tc),(last_n%10+1)*last_tc-(last_n%10*last_tc+last_tg)));
-        }
+        //cout<<st2<<endl;
+        long long temp = toInt(st);
+        printf("Case #%i: %lld",t,min(temp-toInt(st1),toInt(st2)-temp));
         printf("\n");
     }
     return 0;
